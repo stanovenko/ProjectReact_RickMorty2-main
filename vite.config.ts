b9f9f2d17a81732622b976219d950e063b0c5d59
stano <stanovenko@inbox.lv>
2025-05-10 13:25:51 +0300

    third

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ProjectReact_RickMorty/', 
+  base: '/ProjectReact_RickMorty2-main/', 
   plugins: [react()],
 })
